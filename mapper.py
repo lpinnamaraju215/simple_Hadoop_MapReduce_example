9fcd0971d4ddc731f4f356591439d70baa1fda30
Lakshmi Deepika <lpinnamaraju@regis.edu>
2022-05-17 22:34:05 +0000

    lowercase text and remove stopwords

--- mapper.py
+++ mapper.py
@@ -1,14 +1,22 @@
 #!/usr/bin/env python
 import sys
+import string
+
+stop_words = ['the', 'and']
+
+#create translator for mapping punctuation to whitespace
+translator = string.maketrans(string.punctuation, ' '*len(string.punctuation))
 
 # get all lines from stdin
 for line in sys.stdin:
     # remove leading and trailing whitespace
-    line = line.strip()
+    line = line.strip().lower()
+    line = line.translate(translator)
 
     # split the line into words; splits on any whitespace
     words = line.split()
 
     # output tuples (word, 1) in tab-delimited format
     for word in words:
-        print '%s\t%s' % (word, "1")
+        if word not in stop_words:
+            print '%s\t%s' % (word, "1")
